8b9b531d42f24c60cf94f63d74281091446fe3a9
Davier Lopez <davierlopez18dl@gmail.com>
2023-08-01 22:48:29 -0500

    trabajando en el index

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			sans: ['Gotham SSm A, sans-serif']
+		}, minHeight: {
+			'1/2': '200px',
+		},minHeight: {
+			'heigh': '400px',
+		},
+		extend: {},
+	},
+	plugins: [],
+}
